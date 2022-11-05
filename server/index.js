a6ba475eca50259bc7cf575b3b0281444fa9d670
alexissengangabo07 <alexxngabo@gmail.com>
2022-11-05 18:41:41 +0200

    feat: connect database and implement user model

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
+// importing dependencies
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import bcrypt from 'bcrypt';
 
+// config dotenv
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5500;
+const DB_URL = process.env.DATABASE_URL;
 
+// middlewares
+app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-app.listen(PORT, () => console.log(`Server listening at port ${PORT}`));
+// connect database and implementation server listenerr
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Database connected and Server listening at port ${PORT}`));
+    })
+    .catch(err => {
+        console.log(err.message);
+    });
