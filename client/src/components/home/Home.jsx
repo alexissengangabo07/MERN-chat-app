a92c63e4331aef03318255f210e91cb0267cb4fb
alexissengangabo07 <alexxngabo@gmail.com>
2022-11-13 23:43:30 +0200

    style: style chat ui

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AiFillMessage } from 'react-icons/ai';
 import { MdLogout } from 'react-icons/md';
+import { FiSearch } from 'react-icons/fi';
+import { BsThreeDotsVertical } from 'react-icons/bs';
 import { fetchUsers } from '../../feature/users.slice';
 import Loader from '../loader/Loader';
 import Image from './nba yb.jpeg';
@@ -26,19 +28,23 @@ const Home = () => {
         (
           <div className='home-container'>
             <div className='sidebar'>
-              <div>
+              <div className='connected-user-img-container'>
                 <img alt="user-img" src={Image} className='connected-user-img' />
-                <div className='message-icon'>
-                  <AiFillMessage size={40} color='white' />
-                </div>
-                <div className='logout-icon'>
-                  <MdLogout size={40} color='white' />
-                </div>
               </div>
-              <div></div>
-              <div></div>
+              <div className='message-icon'>
+                <AiFillMessage size={40} color='white' />
+              </div>
+              <div className='logout-icon'>
+                <MdLogout size={40} color='white' />
+              </div>
             </div>
-            <div></div>
+            <div className='center-column'>
+              <div>
+                <input type="text" placeholder='search' />
+                <FiSearch />
+                <BsThreeDotsVertical />
+              </div>
+            </div>
             <div></div>
           </div>
         )
