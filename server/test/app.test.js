5c6b05eb24030fc8dcd6995d533e69536e05f53e
alexissengangabo07 <alexxngabo@gmail.com>
2023-01-19 22:01:29 +0200

    test: user login

--- server/test/app.test.js
+++ server/test/app.test.js
@@ -1,2 +1,24 @@
-import supertest from 'supertest';
+import request from 'supertest'
+import app from './app.js'
 
+
+describe("POST /users", () => {
+    describe("given a username and password", () => {
+
+        test("register user account", async () => {
+            const response = await request(app).post("/users").send({
+                email: "email",
+                username: "username",
+                password: "password",
+            })
+            expect(response.statusCode).toBe(201)
+        })
+        test("get user in db", async () => {
+            const response = await request(app).post("/users").send({
+                username: "username",
+                password: "password"
+            })
+            expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+        })
+    })
+});
